0e698027bed5de0b4aff687c739fcbfc77698b70
Leandro <leandropinheiro.pt@hotmail.com>
2026-01-31 20:52:12 +0000

    hello world

---
+++ module00/hello_world/target/debug/deps/hello_world.d
@@ -0,0 +1,5 @@
+C:\Users\Felip\OneDrive\Área de Trabalho\curso prog\Rust-study\module00\hello_world\target\debug\deps\hello_world.d: src\main.rs
+
+C:\Users\Felip\OneDrive\Área de Trabalho\curso prog\Rust-study\module00\hello_world\target\debug\deps\hello_world.exe: src\main.rs
+
+src\main.rs:
